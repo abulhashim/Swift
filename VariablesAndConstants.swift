59f514b3cda65837d98c23637389049016c0ecb1
Ameen A <65440428+abulhashim@users.noreply.github.com>
2021-09-02 19:09:12 +0500

    Variables, Constants, Data Types, ...

---
+++ VariablesAndConstants.swift
@@ -0,0 +1,38 @@
+// Starting Swift at last of August2021
+
+/*  Variables:  */
+
+var str = "String"
+var number = 7
+var decimalNumber = 9.9
+var double = 7.9999
+var bool = true
+str = "Can be Change"
+
+// You can declare multiple variables on a single line, separated by commas,
+var x = 0.0, y = 0.1, z = 0.2
+
+
+/*   Constants:   */
+
+let text = "Can't be Change"
+let isPaid = false
+let isDenied = true
+
+/*   Type Annotation:   */
+var welcomeNote: String
+welcomeNote = "Hello"
+var red, green, blue: Double
+var num: Int
+
+/*    String Interpolation:   */
+
+print("A String \(str)")
+print("An Constant \(text)")
+print("The current value of welcomeNote is \(welcomeNote)")
+
+let sad = "☹️"; print(sad)
+
+let minValue = UInt8.min
+let maxValue = UInt8.max
+print(maxValue)
